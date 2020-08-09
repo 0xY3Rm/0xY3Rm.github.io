5a12998ad0651d905f9059f793805b92fe527cd6
0xY3Rm <0xY3Rm@protonmail.com>
2020-08-09 17:13:28 +0300

    initial release

---
+++ js/0xY3Rm.js
@@ -0,0 +1,6 @@
+//Game
+console.log("I know you were expecting hexadecimal integers after 0x.");
+console.log("Y3Rm means ctf 'capture the flag' in Base64.");
+console.log("Capture The Flag is a kind of information technology competition that challenges contestants to solve a variety of tasks using a shell and programming languages to find the flag, a specific piece of text.");
+console.log("It's one of my hobbies.");
+console.log("Now you know :)");
